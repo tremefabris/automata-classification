fbd93be2338c162993775023b309226e8215e607
Vitor Fabris <fabrisvitor@estudante.ufscar.br>
2022-08-30 22:15:02 -0300

    fix: prints and whatnot

--- conv_cluster.py
+++ conv_cluster.py
@@ -16,7 +16,7 @@ from utils.scaling import *
 from sys import exit
 
 
-def build_model(input_shape):	# AlexNet-inspired model for CA classification
+def build_model(input_shape):
 	model_conv = keras.Sequential()
 	model_conv.add(Input(input_shape))
 
@@ -46,7 +46,7 @@ def build_model(input_shape):	# AlexNet-inspired model for CA classification
 
 
 	model_fc = keras.Sequential()
-	model_fc.add(Dense(units=256, activation='softmax'))	# Segundo a recomendação do Scabini e usando apenas um layer FC
+	model_fc.add(Dense(units=256, activation='softmax'))
 	
 	full_model = keras.Sequential([
 		model_conv,
@@ -70,6 +70,7 @@ def config_argparser():
 	parser.add_argument('--test', action='store_true', help='Se deve-se predictar as regras do conjunto de teste')
 	parser.add_argument('--view', action='store_true', help='Se deve-se visualizar os erros na classificação')
 	parser.add_argument('--kmeans', type=int, metavar='', help='Se deve-se agrupar o conjunto de treino convolucionado com K-means')
+	parser.add_argument('--eca', nargs='+', type=int, metavar='', help='Quais k\'s dos ECAs imprimir')
 	return parser.parse_args()
 
 
@@ -79,12 +80,10 @@ if __name__ == '__main__':
 
 	model, conv_pipe = build_model((120, 120, 1))
 	model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-	print(f'LAYER 0 OF FULL MODEL: {model.get_layer(index=0).name}')
 
 	conv_pipe.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 	conv_pipe.summary()
 
-	input("SAIR POR FAVOR")
 
 	training_generator = NewDataGenerator(dataset_size=102400, n_channels=1)
 	val_generator      = NewDataGenerator(dataset_size=20480, n_channels=1)
@@ -95,19 +94,16 @@ if __name__ == '__main__':
 	cScaler_test  = ContinuousScaler(StandardScaler(), image_shape=(120, 120), n_channels=1)
 
 	for idx, (images, _) in enumerate(training_generator):
-		print(f"batch {idx} being processed")
 		cScaler_train.continuous_train(images)
 	training_generator.on_epoch_end()	# refills datagen
 	training_generator.add_trained_scaler(cScaler_train)
 
 	for idx, (images, _) in enumerate(val_generator):
-		print(f"VAL: batch {idx} being processed")
 		cScaler_val.continuous_train(images)
 	val_generator.on_epoch_end()		# refills datagen
 	val_generator.add_trained_scaler(cScaler_val)
 
 	for idx, (images, _) in enumerate(testing_generator):
-		print(f"TEST: batch {idx} being processed")
 		cScaler_test.continuous_train(images)
 	testing_generator.on_epoch_end()	# refills datagen
 	testing_generator.add_trained_scaler(cScaler_test)
@@ -116,9 +112,6 @@ if __name__ == '__main__':
 		stats = model.fit(x=training_generator, epochs=args.train, validation_data=val_generator,
 	  					  use_multiprocessing=True, workers=6)
 		model.evaluate(x=testing_generator, use_multiprocessing=True, workers=6)
-
-		print("\n\nSAVING MODEL\n\n")
-		model.save('models/alexnet')
 
 		print(f"Std deviation from train loss: {np.std(stats.history['loss'])}")
 		print(f"Std deviation from train acc:  {np.std(stats.history['accuracy'])}")
@@ -131,149 +124,48 @@ if __name__ == '__main__':
 		print(f"Val loss: {stats.history['val_loss']}")
 		print(f"Val acc: {stats.history['val_accuracy']}")
 
-	if args.test:
-		model_test = keras.models.load_model('models/alexnet')
-
-		minibatch   = testing_generator[0]
-		test_images = minibatch[0][:10]
-		test_labels = np.argmax(minibatch[1][:10], axis=1)
-		print(test_images.shape)
-		print(test_labels) 
-
-		results = model_test(test_images, training=False)
-		test_pred = np.argmax(results, axis=1)
-		print(test_pred)
-
-		for idx, img in enumerate(test_images):
-			plot_automata(img, f'real{test_labels[idx]}_pred{test_pred[idx]}')
-
-
+	
 	if args.kmeans is not None:
-		# TODO: Rodar para todo o test set (pegar snippet do resnet.py)
 
-		params = {'batch_size':128, 'dim':(120, 120), 'n_channels':1, 'n_classes':256, 'shuffle':False}
-		partition, labels, folders = load_IDs_rules()
+		eca_generator = NewDataGenerator(dataset_size=256, n_channels=1)
+		cScaler_eca   = ContinuousScaler(StandardScaler(), image_shape=(120, 120), n_channels=1)
+		for idx, (images, _) in enumerate(eca_generator):
+			print(f"ECA: batch {idx} being processed")
+			cScaler_eca.continuous_train(images)
+		eca_generator.on_epoch_end()	# refills datagen
+		eca_generator.add_trained_scaler(cScaler_eca)
+		
+		data_eca_gen = eca_generator[0]
+		imgs_conv_pipe  = data_eca_gen[0]
+		rules_conv_pipe = data_eca_gen[1]
+		eca_feats = conv_pipe.predict(imgs_conv_pipe)
 
-		conv_generator = DataGenerator(partition['test'], labels, folders['test'], data_info['test_mean'], data_info['test_var'], **params)
+		pca         = PCA(0.9, svd_solver='full')
+		data_kmeans = pca.fit_transform(eca_feats)
 
-		data_conv   = conv_generator[0][0]
-		labels_conv = []
-		for batch in conv_generator:
-			labels_conv += np.argmax(batch[1], axis=1).tolist()
-		print(data_conv.shape)
+		print(f"{pca.explained_variance_ratio_=}")
+		print(f"{pca.explained_variance_ratio_.shape=}")
+		print(f"{np.sum(pca.explained_variance_ratio_)=}")
+		print(f"{data_kmeans=}")
 
-		#data  = np.array([])
-		#rules = np.array([])
-		#for i, (imgs, labels) in enumerate(conv_generator):	# Usando test set por causa do menor tamanho
-		#	print(f'batch {i} loaded')
-		#	if i == 0:
-		#		data  = imgs
-		#		rules = labels
-		#	else:
-		#		data  = np.concatenate((data, imgs), axis=0)
-		#		rules = np.concatenate((rules, labels), axis=0)
-		#	print(data.shape)
 
-		print(f'{data.shape=}')
-		print(f'{rules.shape=}')
-
-		conv_dr = conv_pipe.predict(x=conv_generator, use_multiprocessing=True, workers=6)
-		print(conv_dr.shape)
-
-		pca = PCA(45, svd_solver='full')
-		data_pca = pca.fit_transform(conv_dr)
-		print(pca.explained_variance_ratio_)
-		print("shape:", pca.explained_variance_ratio_.shape)
-
-		ratio = pca.explained_variance_ratio_
-		print("\n::::::::STATISTICS::::::::\n")
-		print("sum of components:", np.sum(ratio))
-
-		print(f'{np.argmax(pca.explained_variance_ratio_)}, {pca.explained_variance_ratio_[np.argmax(pca.explained_variance_ratio_)]}')
-		print('\n::::::::::::::::::::::::\n')
-
-		print(data_pca)
-		print(data_pca.shape)
-
-		print('\n::::::::::::::::::::::::\n')
-		print("APROVEITANDO APENAS OS PRIMEIROS 45 COMPONENTES PARA CLUSTERING\n\n")
-
+		kms = np.empty(args.kmeans, dtype=object)
 
 		labels = []
 		inertias = []
-		for k in range(2, args.kmeans):
-			print(f"...initiating kmeans for k = {k}...")	
-			km = KMeans(n_clusters=k, init='k-means++', n_init=100, random_state=42)#.fit(x)
-			km_fit = km.fit_transform(data_pca)
-			labels.append(km.labels_)
-			inertias.append(km.inertia_)
+		for k in range(2, args.kmeans + 1):
+			kms[k - 2] = KMeans(n_clusters=k, init='k-means++', n_init=100, random_state=42, verbose=0)
+			km_fit = kms[k - 2].fit_transform(data_kmeans)
+			labels.append(kms[k - 2].labels_)
+			inertias.append(kms[k - 2].inertia_)
 
+		print(inertias)
 
-		print("labels for k=4:", labels[2])
-		print("shape:", labels[2].shape)
-		print("real labels shape:", len(labels_conv))
-
-		print()
-		print("first 40 kmeans labels:", labels[2][:40])
-		print("first 40 rules:        ", labels_conv[:40])
-
-		print("inertias:", inertias)
-		print("shape:", len(inertias))
-
-		print('\n::::::::::::::::::::::::\n')
-
-		# MAKING A SELECTION SYSTEM FOR THE CLUSTER-CLASSES
-
-		view_number = 100		# Hardcoded just cause
-		k_views = input("QUE k ANALISAR? ").split()
-		k_views = [ int(x) for x in k_views ]
-
-		for k in k_views:
-			view_dict = {}
-
-			for i in range(view_number):
-				if labels[k-2][i] not in view_dict:
-					view_dict[labels[k-2][i]] = []
-				plot_automata(data_conv[i], f'{k}/rule{labels_conv[i]}_class{labels[k-2][i]}')
-				view_dict[labels[k-2][i]].append(labels_conv[i])
-
-			print(f"K = {k}\n")
-
-			for n in view_dict:
-				print(f"CLASSE {n}")
-
-				for x in view_dict[n]:
-					print(x, end=' ')
-				print('\n')
-			print()
-
-
-	if args.view:
-
-		params = {'batch_size':128, 'dim':(120, 120), 'n_channels':1, 'n_classes':256, 'shuffle':False}
-		partition, labels, folders = load_IDs_rules()
-
-		testing_generator = DataGenerator(partition['test'], labels, folders['test'], data_info['test_mean'], data_info['test_var'], **params)
-
-
-		model_pred = keras.models.load_model('models/alexnet')
-		labels_pred = model_pred.predict(x=testing_generator, use_multiprocessing=True, workers=6)
-
-		# Transforming from one-hot to normal list
-		labels_real = []
-		for i, batch in enumerate(testing_generator):
-			labels_real += np.argmax(batch[1], axis=1).tolist()
-		labels_real = np.array(labels_real)
-
-		labels_pred = np.argmax(labels_pred, axis=1)
-		#print(labels_pred.shape)
-
-
-		mislabels = [ i for i, x in enumerate(np.equal(labels_real, labels_pred)) if not x ]
-		for i, index in enumerate(mislabels):
-			print(f"MISLABEL No {i}")
-			print(f"REAL VALUE: {labels_real[index]}")
-			print(f"PRED VALUE: {labels_pred[index]}")
-			print()
-		print(f"TOTAL MISLABELS: {len(mislabels)}")
+		if args.eca is not None:
+			for k in args.eca:
+				print(f"FOR {k} CLUSTERS")
+				for i in range(256):
+					rule = np.argmax(rules_conv_pipe[i])
+					print(f'RULE: {rule}, CLASS: {labels[k - 2][i]}')
+				print('\n\n')
 
--- resnet.py
+++ resnet.py
@@ -27,7 +27,7 @@ def local_plot_automata(autom, name):
 
 def autoencoder(input_shape, embedding_size):
 	img		= keras.layers.Input(shape=input_shape)
-	e 		= keras.layers.Flatten()(img)							# Para a Resnet, serão 2048 pixels de saída
+	e 		= keras.layers.Flatten()(img)
 	e		= keras.layers.BatchNormalization()(e)
 	e 		= keras.layers.Dense(units=2048, activation='relu')(e)
 	e		= keras.layers.Dropout(0.25)(e)
@@ -54,7 +54,6 @@ def autoencoder(input_shape, embedding_size):
 def config_argparser():
 	parser = argparse.ArgumentParser(description="ELEMENTARY CELLULAR AUTOMATA CLASSIFIER (Pretrained ResNet + KMeans)")
 
-	parser.add_argument('--net', type=str, metavar='CNN', required=True, help="Especifica a rede neural a usar para extração de características")
 	parser.add_argument('--kmeans', type=int, metavar='N', help="Realiza o agrupamento com K-means de 2 a N k's")
 
 	exc_group1 = parser.add_mutually_exclusive_group()
@@ -67,30 +66,6 @@ def config_argparser():
 
 	return parser.parse_args()
 
-def dump_rules(clusters, assignments, rules, filename):	# WRAPPER FOR _dump_rules_cluster
-	fd = open(filename, 'wt', encoding='utf-8')
-
-	for k in clusters:
-		fd.write(f"K = {k}\n\n")
-		_dump_rules_cluster(assignments[k - 2], rules, fd)
-		fd.write('\n')
-
-	fd.close()
-
-def _dump_rules_cluster(k_assignments, rules, fd):	# Escreve para todas classes e regras de um único k
-
-	d  = {'cluster':k_assignments, 'rule':rules}
-	df = pd.DataFrame(data=d)
-	classes = df['cluster'].unique()
-
-	for c in np.sort(classes):
-		fd.write(f"CLASS {c}\n")
-		groups = df[ df['cluster'] == c ].to_numpy()
-
-		for group in np.sort(groups[:, 1]):
-			fd.write(f'{group} ')
-		fd.write('\n\n')
-
 
 
 if __name__ == '__main__':
@@ -101,17 +76,12 @@ if __name__ == '__main__':
 	test_ae_generator = NewDataGenerator(dataset_size=51200, n_channels=3)
 
 
-	if args.net.lower() in 'densenet':
-		model = keras.applications.DenseNet121(include_top=False, input_shape=(120, 120, 3), pooling='avg')
-	else:
-		model = keras.applications.ResNet50(include_top=False, input_shape=(120, 120, 3), pooling='avg')
+	model = keras.applications.ResNet50(include_top=False, input_shape=(120, 120, 3), pooling='avg')
 
 	if args.kmeans is not None:
 
-		# Utilizando a nova classe ContinuousScaling
 		cScaler = ContinuousScaler(StandardScaler(), image_shape=(120, 120), n_channels=3)
-		for idx, (images, _) in enumerate(diagram_generator):	# TODO: Para deploy, remover o enumerate
-			print(f"batch {idx} is now processing")
+		for idx, (images, _) in enumerate(diagram_generator):
 			cScaler.continuous_train(images)
 
 		diagram_generator.on_epoch_end()		# refills the datagenerator
@@ -119,30 +89,24 @@ if __name__ == '__main__':
 		
 		extracted_features = model.predict(diagram_generator, use_multiprocessing=True, workers=4, verbose=1)
 
-		print(f"{extracted_features.nbytes=}")
-		print(f"{extracted_features.shape=}")
-		print(f"{extracted_features[0]=}")
-		print()
 
 		if args.ae is not None:
 
 			data_ae = MinMaxScaler().fit_transform(extracted_features)
-			print(f"{data_ae.shape=}")
 
 			autoenc, enc = autoencoder(data_ae.shape[1:], args.ae)
 
 			autoenc.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
-			autoenc.fit(x=data_ae, y=data_ae, batch_size=256, epochs=1000, validation_split=0.1) # Talvez colocar um milhão de autômatos pra treinar aqui
+			autoenc.fit(x=data_ae, y=data_ae, batch_size=256, epochs=500, validation_split=0.1)
 
-			# Avaliando os resultados do autoencoder
 			test_ae_cScaler = ContinuousScaler(StandardScaler(), image_shape=(120, 120), n_channels=3)
 			
-			for idx, (images, _) in enumerate(test_ae_generator):	# TODO: Para deploy, remover o enumerate
-				print(f"TEST: batch {idx} is now processing")
+			for idx, (images, _) in enumerate(test_ae_generator):
 				test_ae_cScaler.continuous_train(images)
 
 			test_ae_generator.on_epoch_end()		# refills the datagenerator
 			test_ae_generator.add_trained_scaler(test_ae_cScaler)
+
 			print("Autoencoder evaluate")
 			test_extfeat = model.predict(test_ae_generator, use_multiprocessing=True, workers=4, verbose=1)
 			test_feedae  = MinMaxScaler().fit_transform(test_extfeat)
@@ -150,73 +114,44 @@ if __name__ == '__main__':
 
 			data_kmeans = enc.predict(data_ae, batch_size=256, verbose=1)
 
-			print(f"{data_kmeans.shape=}")
-			#exit()
 
 		if args.pca is not None:
 			pca 	    = PCA(0.9, svd_solver='full')
-			data_kmeans = pca.fit_transform(extracted_features)		# Uai, agora tá dando 14 componentes kkkkkkk
+			data_kmeans = pca.fit_transform(extracted_features)
 
 			print(f"{pca.explained_variance_ratio_=}")
 			print(f"{pca.explained_variance_ratio_.shape=}")
 			print(f"{np.sum(pca.explained_variance_ratio_)=}")
 			print(f"{data_kmeans=}")
 
-		#input("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
 
-		# K-means per se
-		# Passei a armazenar todos os runs do K-means em um vetor pra poder usar depois pros ECAs
 		kms = np.empty(args.kmeans, dtype=object)
 
 		labels = []
 		inertias = []
 		for k in range(2, args.kmeans + 1):
-			print(f"...initiating kmeans for {k} clusters...")
 			kms[k - 2] = KMeans(n_clusters=k, init='k-means++', n_init=100, random_state=42, verbose=0)
-			km_fit = kms[k - 2].fit_transform(data_kmeans)		# Podia estar usando fit_predict()
+			km_fit = kms[k - 2].fit_transform(data_kmeans)
 			labels.append(kms[k - 2].labels_)
 			inertias.append(kms[k - 2].inertia_)
 
-		print(labels)
-		print(inertias)
 
-		#input("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-
-		if args.clusters is not None:	# Ou rodo pros clusters do testset, ou rodo pros 256 ECAs
-
-			view_max = 512
-			for k in args.clusters:
-				for i in range(view_max):
-					group = labels[k-2][i]		# k - 2 pois começo o k-means em k = 2
-					rule  = rules[i]
-					plot_automata(automs[i], f'cluster/{k}/rule{rule}_class{group}')
-
-			dump_rules(args.clusters, labels, rules, 'data_transfer/assigned_labels_cluster.txt')
-			print("\nIMAGES SAVED\n")
-
-		# Fluxo para rodar os 256 Elementares
 		if args.eca is not None:
 			if args.pca is not None:
-				save_dir = 'resnet/pca'		# plot_automata já inclui data_transfer como diretório inicial
+				save_dir = 'resnet/pca'
 			elif args.ae is not None:
 				save_dir = 'resnet/ae'
 
-			print()
-			print("SENHORAS E SENHORES")
-			print("AGORA É HORA")
-			print("DOS EEEEEEECAAAAAAAAAAAAAAS")
-			print()
 			
+			eca_data = eca_generator[0]
+			eca_imgs = eca_data[0]
+			rules    = np.argmax(eca_data[1], axis=1)
 
-			eca_cScaler = ContinuousScaler(StandardScaler(), image_shape=(120, 120), n_channels=3)
-			for idx, (images, _) in enumerate(eca_generator):	# TODO: Para deploy, remover o enumerate
-				print(f"ECA: batch {idx} is now processing")
-				eca_cScaler.continuous_train(images)
+			eca_scaled = np.reshape(eca_imgs, (eca_imgs.shape[0], -1))
+			eca_scaled = StandardScaler().fit_transform(eca_scaled)
 
-			eca_generator.on_epoch_end()		# refills the datagenerator
-			eca_generator.add_trained_scaler(cScaler)
-		
-			eca_features = model.predict(eca_generator, use_multiprocessing=True, workers=4, verbose=1)
+			eca_scaled = np.reshape(eca_scaled, (-1, 120, 120, 3))
+			eca_features = model.predict(eca_scaled, verbose=1)
 
 			if args.ae is not None:
 				data_ae = MinMaxScaler().fit_transform(eca_features)
@@ -225,12 +160,6 @@ if __name__ == '__main__':
 			if args.pca is not None:
 				data_eca = pca.transform(eca_features)
 
-			# TODO: Salvar as regras dos autômatos para plottar (pqp vei)
-
-			# Salva as características extraídas pela ResNet dos 256 ECAs
-			np.savez(f'data_transfer/{save_dir}/resnet_feat_extraction.npz', feats=eca_features, rules=rules)
-			if args.eca is not None:
-				np.savez(f'data_transfer/{save_dir}/ae_dr.npz', embedding=data_eca, rules=rules)
 
 			eca_labels = []
 			for k in range(2, args.kmeans):
@@ -241,12 +170,9 @@ if __name__ == '__main__':
 			print(rules)
 
 			for k in args.eca:
-				print(f"PLOTTING FOR {k} CLUSTERS")
 				for i in range(256):
 					group = eca_labels[k-2][i]		# k - 2 pois começo o k-means em k = 2
 					rule  = rules[i]
-					plot_automata(gray_automs[i], f'{save_dir}/{k}/rule{rule}_class{group}')
+					plot_automata(eca_imgs[i], f'{save_dir}/{k}/rule{rule}_class{group}')
 			print("\nIMAGES SAVED\n")
-
-			dump_rules(args.eca, eca_labels, rules, f'data_transfer/{save_dir}/assigned_labels.txt')
 
